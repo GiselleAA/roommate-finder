4f2f6ff439a55c25a58dfe0391b364a2c17af71e
thatfoogabriel <sotogabriel1228@gmail.com>
2023-03-26 19:54:07 -0700

    client-server setup

---
+++ src/Client/App.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+  const apiCall{
+    axios.get('http://localhost:8080').then(() => {
+      console.log('wee!');
+    })
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <button onClick={apiCall}>Make API Call</button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
